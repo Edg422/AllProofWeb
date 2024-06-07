0339fafa3b5fcf6fd2984f8c379126a445f6178c
Edgard <eperezpalmav@uni.pe>
2024-06-07 14:06:20 -0500

    3D Verge

--- allproof/allproof_web/views.py
+++ allproof/allproof_web/views.py
@@ -6,18 +6,3 @@ def index(request):
 
 def allproof_web(request):
     return render(request, 'allproof_web/templates/allproof_web/allproof_web.html')
-
-def gltf(request):
-    return redirect('/static/verge/Allproof_web.gltf')
-
-def visual(request):
-    return redirect('/static/verge/visual_logic.js')
-
-def bin(request):
-    return redirect('/static/verge/Allproof_web.bin')
-
-def png(request):
-    return redirect('/static/verge/path6864.png')
-
-def autoshop(request):
-    return redirect('/static/verge/autoshop_01_1k.png')
