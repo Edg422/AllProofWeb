c345335a50513b82bb3677a5b6d7ccc473448b42
Edgard <eperezpalmav@uni.pe>
2024-06-07 14:02:21 -0500

    3D Verge

--- allproof/allproof_web/urls.py
+++ allproof/allproof_web/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('allproof_web', views.allproof_web, name='allproof_web'),
     #path('Allproof_web.gltf', views.gltf, name='gltf'),
     #path('visual_logic.js', views.visual, name='visual'),
-    path('Allproof_web.bin', views.bin, name='bin'),
-    path('path6864.png', views.png, name='png'),
-    path('autoshop_01_1k.png', views.autoshop, name='autoshop'),
+    #path('Allproof_web.bin', views.bin, name='bin'),
+    #path('path6864.png', views.png, name='png'),
+    #path('autoshop_01_1k.png', views.autoshop, name='autoshop'),
 ]
